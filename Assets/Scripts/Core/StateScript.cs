500564702850c1591f4e33dac299e71c94521bd5
SirYakalot <asher.einhorn@gmail.com>
2016-12-25 23:33:00 +0000

    State Script + .gitignore
    First pass state script - untested

---
+++ Assets/Scripts/Core/StateScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class StateScript : MonoBehaviour {
+
+	protected delegate void UpdateFunc();
+	protected delegate IEnumerator StartFunc ();
+
+	UpdateFunc updateFunc;
+	StartFunc  startFunc;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		updateFunc();
+	}
+
+	protected void Go(StartFunc newStart, UpdateFunc newUpdate)
+	{
+		StopCoroutine (startFunc());
+		startFunc = newStart;
+		StartCoroutine (newStart());
+
+		updateFunc = newUpdate;
+	}
+}
