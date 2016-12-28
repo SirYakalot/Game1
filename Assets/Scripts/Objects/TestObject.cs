b8c1d4a55052f1f4f2cef8955cdcffb53d9429d5
SirYakalot <asher.einhorn@gmail.com>
2016-12-28 21:35:31 +0000

    working test intractable

---
+++ Assets/Scripts/Objects/TestObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : Interactable {
+
+	protected override void RequestedInteract ()//what is 'final'?
+	{
+		base.RequestedInteract ();//don't really need to do this
+
+		print ("boink");//turn this into a nice object system
+	}
+}
