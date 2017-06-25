5f37b8545ab6f59486e7e931225ae4866b3c4f6b
Asher Einhorn <asher.einhorn@gmail.com>
2017-06-24 18:22:27 -0700

    working out springs

--- Assets/Scripts/Core/ScriptFuncs.cs
+++ Assets/Scripts/Core/ScriptFuncs.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public static class ScriptFuncs
 {
-	public static float LerpScale(float lowIn, float highIn, float lowOut, float highOut, float t)
+    // Maths----------------------------------
+
+    public static float LerpScale(float lowIn, float highIn, float lowOut, float highOut, float t)
 	{
 		return Mathf.Lerp (lowOut, highOut, Mathf.InverseLerp (lowIn, highIn, t));//test this
 	}
 
-	//this should extend Vector3
-	public static Vector3 FlattenY (Vector3 vector)
-	{
-		return new Vector3 (vector.x, 0.0f, vector.z);
-	}
+    //this should extend Vector3
+    public static Vector3 FlattenY(Vector3 vector)
+    {
+        return new Vector3(vector.x, 0.0f, vector.z);
+    }
+
+    //kStrength of 1 would move the spring back to the start pos every frame. < 1 more gradual.
+    public static float Spring(float xDistFromStart, float kStrength)
+    {
+        float fForce = -kStrength * xDistFromStart;
+        return fForce;
+    }
+
+    // AI stuff------------------------------
 		
 	public static npc GetNearestNpc()
 	{
---
+++ Assets/Scripts/SpringCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringCamera : MonoBehaviour {
+
+    public object focus;
+    private Transform startLoc;
+    private float temp;
+	// Use this for initialization
+	void Start () {
+        startLoc = transform;
+        temp = transform.position.x + 3.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float distFromSpringPos = transform.position.x - temp;
+        float delta = ScriptFuncs.Spring(distFromSpringPos, 1.0f);
+        delta *= Time.deltaTime;
+
+        transform.position = new Vector3(transform.position.x + delta, transform.position.y, transform.position.z);
+	}
+}
