50c41745f34a561f26707c808357fb906793f117
Asher Einhorn <asher.einhorn@gmail.com>
2017-07-19 21:31:02 -0700

    Proper damping

--- Assets/Scripts/SpringCamera.cs
+++ Assets/Scripts/SpringCamera.cs
@@ -8,9 +8,9 @@ public class SpringCamera : MonoBehaviour
     private Vector3 prevPos;
     private float speed = 0.0f;
 
-    public float strength = 0.0f;
-    public float mass = 0.0f;
-    public float damping = 0.0f;
+    public float strength = 1.0f;
+    public float mass = 1.0f;
+    public float damping = 1.0f;
 
     private void Start()
     {
@@ -19,7 +19,7 @@ public class SpringCamera : MonoBehaviour
 
     private void Update()
     {
-        float velocity = Vector3.Magnitude(transform.position - prevPos);
+        float velocity = (transform.position - prevPos).x;
         print(velocity);
         prevPos = transform.position;
         speed = ScriptFuncs.SpringObjectLinear(speed, springAnchor, this.gameObject, strength, mass, damping, velocity);
