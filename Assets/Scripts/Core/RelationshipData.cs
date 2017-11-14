d362d89c83710d3258a2eb5aa6c6fc0c39feff75
Asher Einhorn <asher.einhorn@gmail.com>
2017-11-14 08:51:37 -0800

    some getters in relationsip class

--- Assets/Scripts/Core/RelationshipData.cs
+++ Assets/Scripts/Core/RelationshipData.cs
@@ -10,6 +10,19 @@ public class RelationshipData
     //allow both
     public bool like;
     public bool hate;
+    
+    // these lines should ultimately come from the npc - so, they each have a hate line, a like line etc. differences. 
+    // what if the sentence is somehow composed of that, and some sescriptive words found in the target NPC, just to provide enough randomness to not be repetitive. in a way 'person' should be in the target. - don't have to worry about having 'types', each npc decribes who they are. 
+
+    public string Like
+    {
+        get { return like ? "I really like them." : "I don't like them."; }
+    }
+
+    public string Hate
+    {
+        get { return hate ? "I fuckin' hate that person." : "No, I don't mind that person."; }
+    }
 
     public RelationshipData(string name)
     {
