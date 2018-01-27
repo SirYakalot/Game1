972ed4e0abcd64891dc751e2b65111615af13152
Asher Einhorn <asher.einhorn@gmail.com>
2018-01-27 15:37:51 -0800

    cam updates
    added ability for camera to look at anothe robject while keeping player
    in view

--- Assets/Scripts/Character/Controller.cs
+++ Assets/Scripts/Character/Controller.cs
@@ -10,6 +10,7 @@ public class Controller : MonoBehaviour
 	public float jumpSpeed = 1.0F;
 	public float gravity = 20.0F;
 	public AnimationCurve jumpCurve;
+    public Transform m_Camera; 
 
     private Vector3 moveDirection = Vector3.zero;
 	private float jumpPhase = 0.0f;
@@ -23,8 +24,6 @@ public class Controller : MonoBehaviour
 
 	public MovementState m_State = MovementState.falling;
 
-	public Transform thisCamera;
-
 	private float jumpOffPoint;
 
 	void FixedUpdate() 
@@ -36,6 +35,7 @@ public class Controller : MonoBehaviour
 		{
             case MovementState.grounded:
                 PlayerMove(1.0f);
+                ApplyRotation();
 
                 if (Input.GetButton("Jump"))
                 {
@@ -47,6 +47,8 @@ public class Controller : MonoBehaviour
                     m_State = MovementState.falling;
                 break;
             case MovementState.jumping:
+                   //controller doesn't rotate in the air, should skid out if there's a lot of forward momenntum
+
                 PlayerMove(0.2f);
 
                 jumpPhase += Time.deltaTime * jumpSpeed;
@@ -55,7 +57,7 @@ public class Controller : MonoBehaviour
                 float movementDelta = nextPos - transform.position.y;
 
                 //moveDirection takes a delta
-			   // moveDirection.y = movementDelta;
+                // moveDirection.y = movementDelta;
                 transform.position = new Vector3(transform.position.x, jumpOffPoint + jumpCurve.Evaluate(jumpPhase), transform.position.z);
 
                 if (controller.isGrounded)
@@ -67,7 +69,7 @@ public class Controller : MonoBehaviour
                     m_State = MovementState.falling;
                 }
                 break;
-		    case MovementState.falling:
+            case MovementState.falling:
                 //rewrite to take curve into account
 			    moveDirection.y -= gravity * Time.deltaTime;
 
@@ -82,10 +84,16 @@ public class Controller : MonoBehaviour
 		controller.Move(moveDirection * Time.deltaTime);
 	}
 
+    private void ApplyRotation()
+    {
+        Vector3 eulerCamRotation = m_Camera.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(0.0f, eulerCamRotation.y, 0.0f);
+    }
+
     private void PlayerMove(float speedMulitplier)
     {
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        moveDirection = Quaternion.LookRotation(ScriptFuncs.FlattenY(thisCamera.transform.forward)) * moveDirection;
+        moveDirection = Quaternion.LookRotation(ScriptFuncs.FlattenY(m_Camera.transform.forward)) * moveDirection;
 
         moveDirection *= (speed * speedMulitplier);
     }
--- Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
+++ Assets/Standard Assets/Cameras/Scripts/FreeLookCam.cs
@@ -13,6 +13,7 @@ namespace UnityStandardAssets.Cameras
         // 		Pivot
         // 			Camera
 
+        
         [SerializeField] private float m_MoveSpeed = 1f;                      // How fast the rig will move to keep up with the target's position.
         [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f;   // How fast the rig will rotate from user input.
         [SerializeField] private float m_TurnSmoothing = 0.0f;                // How much smoothing to apply to the turn input, to reduce mouse-turn jerkiness
@@ -20,6 +21,8 @@ namespace UnityStandardAssets.Cameras
         [SerializeField] private float m_TiltMin = 45f;                       // The minimum value of the x axis rotation of the pivot.
         [SerializeField] private bool m_LockCursor = false;                   // Whether the cursor should be hidden and locked.
         [SerializeField] private bool m_VerticalAutoReturn = false;           // set wether or not the vertical axis should auto return
+        [SerializeField] private bool m_CamLookAtMode = false;
+        [SerializeField] protected Transform m_CamLookAtTarget;               // The target object to follow
 
         private float m_LookAngle;                    // The rig's y axis rotation.
         private float m_TiltAngle;                    // The pivot's x axis rotation.
@@ -66,21 +69,22 @@ namespace UnityStandardAssets.Cameras
             transform.position = Vector3.Lerp(transform.position, m_Target.position, deltaTime*m_MoveSpeed);
         }
 
-
         private void HandleRotationMovement()
         {
-			if(Time.timeScale < float.Epsilon)
-			return;
+            if (Time.timeScale < float.Epsilon)
+                return;
 
-            // Read the user input
-            var x = CrossPlatformInputManager.GetAxis("Mouse X");
+            //// Read the user input
+            //var x = CrossPlatformInputManager.GetAxis("Mouse X");
             var y = CrossPlatformInputManager.GetAxis("Mouse Y");
 
-            // Adjust the look angle by an amount proportional to the turn speed and horizontal input.
-            m_LookAngle += x*m_TurnSpeed;
+            //// Adjust the look angle by an amount proportional to the turn speed and horizontal input.
+            //m_LookAngle += x * m_TurnSpeed;
 
-            // Rotate the rig (the root object) around Y axis only:
-            m_TransformTargetRot = Quaternion.Euler(0f, m_LookAngle, 0f);
+            //// Rotate the rig (the root object) around Y axis only:
+            //m_TransformTargetRot = Quaternion.Euler(0f, m_LookAngle, 0f);
+
+            m_TransformTargetRot = Quaternion.LookRotation(m_CamLookAtTarget.position - m_Target.position);
 
             if (m_VerticalAutoReturn)
             {
@@ -92,24 +96,24 @@ namespace UnityStandardAssets.Cameras
             else
             {
                 // on platforms with a mouse, we adjust the current angle based on Y mouse input and turn speed
-                m_TiltAngle -= y*m_TurnSpeed;
+                m_TiltAngle -= y * m_TurnSpeed;
                 // and make sure the new value is within the tilt range
                 m_TiltAngle = Mathf.Clamp(m_TiltAngle, -m_TiltMin, m_TiltMax);
             }
 
             // Tilt input around X is applied to the pivot (the child of this object)
-			m_PivotTargetRot = Quaternion.Euler(m_TiltAngle, m_PivotEulers.y , m_PivotEulers.z);
+            m_PivotTargetRot = Quaternion.Euler(m_TiltAngle, m_PivotEulers.y, m_PivotEulers.z);
 
-			if (m_TurnSmoothing > 0)
-			{
-				m_Pivot.localRotation = Quaternion.Slerp(m_Pivot.localRotation, m_PivotTargetRot, m_TurnSmoothing * Time.deltaTime);
-				transform.localRotation = Quaternion.Slerp(transform.localRotation, m_TransformTargetRot, m_TurnSmoothing * Time.deltaTime);
-			}
-			else
-			{
-				m_Pivot.localRotation = m_PivotTargetRot;
-				transform.localRotation = m_TransformTargetRot;
-			}
+            if (m_TurnSmoothing > 0)
+            {
+                m_Pivot.localRotation = Quaternion.Slerp(m_Pivot.localRotation, m_PivotTargetRot, m_TurnSmoothing * Time.deltaTime);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, m_TransformTargetRot, m_TurnSmoothing * Time.deltaTime);
+            }
+            else
+            {
+                m_Pivot.localRotation = m_PivotTargetRot;
+                transform.localRotation = m_TransformTargetRot;
+            }
         }
     }
 }
