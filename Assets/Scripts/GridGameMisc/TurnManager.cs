9445e239cef6fd9afbf07bfc38b15e88c2bea709
Asher <asher.einhorn@gmail.com>
2019-01-02 22:42:41 +0000

    Some unfinished work on turn manager

--- Assets/Scripts/GridGameMisc/TurnManager.cs
+++ Assets/Scripts/GridGameMisc/TurnManager.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TurnManager : StateScript {
+public class TurnManager : StateScript, IPointerClickHandler
+{
     public bool turnComplete = false;
     public bool playerTurn = true;
 	// Use this for initialization
@@ -11,17 +13,28 @@ public class TurnManager : StateScript {
 	}
 	private IEnumerator PlayerTurn()
     {
+        // this should all be wrappd in a gridchar func really...
+        // need some grid game globals. magic lists. can't be fucked to do this right now
+        thingClicked.gameObject.GetComponent<GridCharacter>().UsedThisTurn = true;
+        thingClicked.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
         playerTurn = true;
         turnComplete = false;
         yield return new WaitUntil(() => turnComplete);
         Go(EnemyTurn(), null);
     }
 
+    //Detect if a click occurs
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        turnComplete = true;
+    }
+
     private IEnumerator EnemyTurn()
     {
         playerTurn = false;
         turnComplete = false;
-        yield return new WaitUntil(() => turnComplete);
+        // yield return new WaitUntil(() => turnComplete);
         Go(PlayerTurn(), null);
+        yield return 0;
     }
 }
