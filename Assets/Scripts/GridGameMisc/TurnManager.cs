fcf32bf31fd10a84cfb8119c1a8012a2a9b635c1
SirYakalot <asher.einhorn@gmail.com>
2019-12-09 18:51:48 +0000

    Unbroke the clicker somehow

--- Assets/Scripts/GridGameMisc/TurnManager.cs
+++ Assets/Scripts/GridGameMisc/TurnManager.cs
@@ -1,40 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.EventSystems;
 
-public class TurnManager : StateScript, IPointerClickHandler
-{
-    public bool turnComplete = false;
-    public bool playerTurn = true;
-	// Use this for initialization
-	void Start () {
-		Go(PlayerTurn(), null);
-	}
-	private IEnumerator PlayerTurn()
-    {
-        // this should all be wrappd in a gridchar func really...
-        // need some grid game globals. magic lists. can't be fucked to do this right now
-        thingClicked.gameObject.GetComponent<GridCharacter>().UsedThisTurn = true;
-        thingClicked.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-        playerTurn = true;
-        turnComplete = false;
-        yield return new WaitUntil(() => turnComplete);
-        Go(EnemyTurn(), null);
-    }
+// public class TurnManager : StateScript, IPointerClickHandler
+// {
+//     public bool turnComplete = false;
+//     public bool playerTurn = true;
+// 	// Use this for initialization
+// 	void Start () {
+// 		Go(PlayerTurn(), null);
+// 	}
+// 	private IEnumerator PlayerTurn()
+//     {
+//         // this should all be wrappd in a gridchar func really...
+//         // need some grid game globals. magic lists. can't be fucked to do this right now
+//         thingClicked.gameObject.GetComponent<GridCharacter>().UsedThisTurn = true;
+//         thingClicked.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+//         playerTurn = true;
+//         turnComplete = false;
+//         yield return new WaitUntil(() => turnComplete);
+//         Go(EnemyTurn(), null);
+//     }
 
-    //Detect if a click occurs
-    public void OnPointerClick(PointerEventData pointerEventData)
-    {
-        turnComplete = true;
-    }
+//     //Detect if a click occurs
+//     public void OnPointerClick(PointerEventData pointerEventData)
+//     {
+//         turnComplete = true;
+//     }
 
-    private IEnumerator EnemyTurn()
-    {
-        playerTurn = false;
-        turnComplete = false;
-        // yield return new WaitUntil(() => turnComplete);
-        Go(PlayerTurn(), null);
-        yield return 0;
-    }
-}
+//     private IEnumerator EnemyTurn()
+//     {
+//         playerTurn = false;
+//         turnComplete = false;
+//         // yield return new WaitUntil(() => turnComplete);
+//         Go(PlayerTurn(), null);
+//         yield return 0;
+//     }
+// }
